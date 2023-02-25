9d03780878b6231f34aa82c945014f19f57252f2
tosh7 <zlia.6.lj.425@gmail.com>
2023-02-25 13:51:47 +0900

    Initial Commit

---
+++ RichTextField/RichTextField.h
@@ -0,0 +1,18 @@
+//
+//  RichTextField.h
+//  RichTextField
+//
+//  Created by Satoshi Komatsu on 2023/02/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RichTextField.
+FOUNDATION_EXPORT double RichTextFieldVersionNumber;
+
+//! Project version string for RichTextField.
+FOUNDATION_EXPORT const unsigned char RichTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RichTextField/PublicHeader.h>
+
+
