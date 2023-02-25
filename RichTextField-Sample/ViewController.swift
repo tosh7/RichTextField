4d9bc1c6a422ad7fa1bf2b7cbe66e04b31a0c261
tosh7 <zlia.6.lj.425@gmail.com>
2023-02-25 14:01:55 +0900

    Create sanmple

---
+++ RichTextField-Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RichTextField-Sample
+//
+//  Created by Satoshi Komatsu on 2023/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
